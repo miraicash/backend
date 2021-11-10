2701e0c3445acfbac832d15b274d1d17df199bee
Jahaan Jain <jahaanjain@gmail.com>
2021-11-10 12:16:31 -0500

    changed default endpoint message to json

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const usersRouter = require("./routes/users");
 app.use("/users", usersRouter);
 
 app.get("/", (req, res) => {
-    res.send("API Route works.");
+    try {
+        res.json({ message: "API Route working." });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
